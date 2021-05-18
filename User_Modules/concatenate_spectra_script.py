2dbbbb084e37cf166042a9c1adffd7415e9da936
Poky <46821344+pokynmr@users.noreply.github.com>
2021-05-18 14:49:00 -0600

    Concatenate two spectra into one.

--- User_Modules/concatenate_spectra_script.py
+++ User_Modules/concatenate_spectra_script.py
@@ -1,9 +1,9 @@
 #
 # This is an example script for concatenating two spectra.
-# Two spectra should share the same ni, spectral width, etc.
+# Spectra will be chosen from the running Poky session.
 #
 # Developed by Woonghee Lee, Ph.D. (woonghee.lee@ucdenver.edu)
-# Last update: May 12, 2021
+# Last update: May 18, 2021
 #
 # To run this script:
 #   In Poky Notepad,
@@ -31,6 +31,8 @@ sp2 = name_to_spectrum(specname2, s)
 if sp2 == None:
   raise SystemError
 
+scale_offset = np.array(sp.scale_offset) - np.array(sp2.scale_offset)
+
 import os.path
 new_path = s.save_filedialog('New spectrum name', 'UCSF (*.ucsf);; Any (*)',
                                   os.path.dirname(sp.data_path))
@@ -48,11 +50,18 @@ proj = s.project
 
 dic, data = ng.sparky.read_lowmem(sp.data_path)
 dic2, data2 = ng.sparky.read_lowmem(sp2.data_path)
+data3 = np.array(data2)
 
-data3 = np.array(data) + factor*np.array(data2)
+# apply offset differences
+ppm_per_pixel = np.array(sp.spectrum_width) / np.array(sp.data_size)
+pixel_offset = scale_offset / ppm_per_pixel
+
+for i in range(sp.dimension):
+  np.roll(data3, int(pixel_offset[i]), axis=i)
+
+data3 = np.array(data) + factor*np.array(data3)
 
 ng.sparky.write_lowmem(new_path, dic, data3, overwrite=True)
 
 if s.show_message_yes_no('Load data', 'Do you want to load the concatenated spectrum?'):
   s.open_spectrum(new_path)
-
