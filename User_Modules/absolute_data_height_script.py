5ca7bd5338a59197dd833e52f3105354f9d1f64b
Poky <46821344+pokynmr@users.noreply.github.com>
2022-04-21 06:53:50 -0600

    Make data height absolute values.

---
+++ User_Modules/absolute_data_height_script.py
@@ -0,0 +1,53 @@
+#
+# This is an example script for making the data height absolute 
+# selected spectrum in the project.
+#
+# Developed by Woonghee Lee, Ph.D. (woonghee.lee@ucdenver.edu)
+# Last update: April 21, 2022
+#
+# To run this script:
+#   In Poky Notepad,
+#     File -> Run Python Module
+#
+# POKY 01/14/22e or higher is required to run this script.
+#
+
+import numpy as np
+from sputil import name_to_spectrum
+# POKY libraries
+import __main__
+s = __main__.main_session
+specname = s.show_spectrumselectiondialog('Select a spectrum', 0)
+sp = name_to_spectrum(specname, s)
+
+if sp == None:
+  raise SystemError
+
+import os.path
+new_path = s.save_filedialog('New spectrum name', 'UCSF (*.ucsf);; Any (*)',
+                                  os.path.dirname(sp.data_path))
+if new_path == '':
+  raise SystemError
+
+transform_engine = s.show_selectionexdialog('Engine selection', 
+    'Engine for the transformation: ', 
+    ('UCSFTOOL', 'NMRGLUE', 'CANCEL'))
+
+if transform_engine == 2:
+  raise SystemError
+elif transform_engine == 0:
+  #### THIS IS UCSFTOOL VERSION
+  import ucsftool3
+  ut = ucsftool3.ucsfTool()
+  ut.ucsf_open(sp.data_path)
+  ut.write_transform(new_path, 'abs', 0, 0, overwrite=1)
+  ut.ucsf_close()
+elif transform_engine == 1:
+  #### THIS IS NMRGLUE VERSION
+  import nmrglue as ng
+  dic, data = ng.sparky.read_lowmem(sp.data_path)
+  new_data = np.absolute(data)
+  ng.sparky.write_lowmem(new_path, dic, new_data, overwrite=True)
+
+if s.show_message_yes_no('Load data', 'Do you want to load the processed spectrum?'):
+  s.open_spectrum(new_path)
