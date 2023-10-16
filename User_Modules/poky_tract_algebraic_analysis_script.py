379118ad47d59f56c2614d47524a7d267f91954b
Poky <46821344+pokynmr@users.noreply.github.com>
2023-10-16 14:34:56 -0600

    Add files via upload

---
+++ User_Modules/poky_tract_algebraic_analysis_script.py
@@ -0,0 +1,91 @@
+#
+# This is an example script for performing TRACT algebraic analysis.
+#   by Woonghee Lee, Ph.D. (woonghee.lee@ucdenver.edu)
+#
+#   Original code by Scott A. Robson, Ph.D.
+#
+# To run this script:
+#   In Poky Notepad,
+#     File -> Run Python Module
+#     and select a directory that has fid file in.
+#
+# Reference:
+#   Scott A. Robson, Çağdaş Dağ, Hongwei Wu & Joshua J. Ziarek
+#     TRACT revisited: an algebraic solution for determining overall 
+#     rotational correlation times from cross-correlated relaxation rates. 
+#     J Biomol NMR 75, 293–302 (2021). 
+#     https://doi.org/10.1007/s10858-021-00379-5
+#
+#   https://github.com/nomadiq/TRACT_analysis
+#
+
+
+import numpy as np
+import __main__
+s = __main__.session
+
+# field in MHz
+try:
+    field = float(s.show_inputdialog('Field in MHz', 
+                                     'Type magnetic strength (MHz)', '750'))    
+except:
+    s.show_message('Error', 'Value must be numberic.')
+    raise SystemError
+
+# measured relaxation rates
+try:
+    Rb = float(s.show_inputdialog('Relaxation Rates', 
+                                     'Measured relaxation rate Rb (Hz) ?', '64'))    
+except:
+    s.show_message('Error', 'Value must be numberic.')
+    raise SystemError
+
+try:
+    Ra = float(s.show_inputdialog('Relaxation Rates', 
+                                     'Measured relaxation rate Ra (Hz) ?', '13'))    
+except:
+    s.show_message('Error', 'Value must be numberic.')
+    raise SystemError
+
+
+# constants
+h = 6.62607004 * (1/np.power(10,34, dtype=np.longdouble))    # Plank's
+mu_0 = 1.25663706 * (1/np.power(10,6, dtype=np.longdouble))  # vacuum permeability
+gamma_H = 267.52218744 * np.power(10,6, dtype=np.longdouble) # proton gyromagnetic ratio
+gamma_N = -27.116 * np.power(10,6, dtype=np.longdouble)      # 15N gyromagnetic ratio
+r = 1.02 * (1/np.power(10,10, dtype=np.longdouble))          # internuclear distance
+delta_dN = 160 * (1/np.power(10, 6))                         # diff in axially symetric 15N CS tensor
+theta = 17*np.pi/180                                         # angle between CSA axis and N-H bond
+
+
+# derived field value in Tesla
+B_0 = field * np.power(10, 6, dtype=np.longdouble) * 2 * np.pi / gamma_H # in Tesla
+
+# equation (5)
+p = mu_0*gamma_H*gamma_N*h/(16*np.pi*np.pi*np.sqrt(2)*np.power(r,3))     # DD 1H-15N bond
+
+# equation (6)
+dN = gamma_N*B_0*delta_dN/(3*np.sqrt(2))                                 # 15N CSA
+
+# equation (7)
+w_N = B_0 * gamma_N                                                      # 15N frequency (radians/s)
+
+# from equivalence between equation (8) RHS and equation (9) RHS
+c = (Rb - Ra)/(2*dN*p*(3*np.cos(theta)**2-1))
+
+# function that returns evaluation of equation (10) with w_N and constant 'c' above as inputs
+def tc(w_N, c):
+    
+    t1 = (5*c)/24 
+    t2 = (336*(w_N**2) - 25*(c**2)*(w_N**4)) / (24*(w_N**2) * (1800*c*(w_N**4) + 125*(c**3)*(w_N**6) + 24*np.sqrt(3)*np.sqrt(21952*(w_N**6) - 3025*(c**2)*(w_N**8) + 625*(c**4)*(w_N**10)))**(1/3))
+    t3 = (1800*c*(w_N**4) + 125*(c**3)*(w_N**6) + 24*np.sqrt(3)*np.sqrt(21952*(w_N**6) - 3025*(c**2)*(w_N**8) + 625*(c**4)*(w_N**10)))**(1/3)/(24*w_N**2) 
+    
+    return t1 - t2 + t3
+
+# computation of result from panel A of figure 4 in Lee et al. 
+tau_c = tc(w_N, c)
+msg = f'Given: field = {field} MHz, Rb = {Rb} Hz and Ra = {Ra} Hz\n' + \
+      f'tau_c: {tau_c} seconds -- using algebraic solution'
+
+print(msg)
+s.show_message('Results', msg)
